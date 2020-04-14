5b238138419a5479ca68028362f2f2cd512a3076
Charly1046 <corderodedios182@gmail.com>
2020-04-14 00:01:05 -0500

    Base_ROAS_v4 (validacion de cruzes)

--- Analytics/librerias/analytics.py
+++ Analytics/librerias/analytics.py
@@ -16,33 +16,86 @@ import plotly.express as px
 import plotly.graph_objects as go
 import glob
 import random
-
+analysis = 'segmentos_intencion_compra'
+nivel = 'Audiencia'
+Cuenta = ['Office Depot']
+i = 0
 pd.set_option('display.float_format', lambda x: '%.f' % x)
 
 #--          UNION DE LOS DIFERENTES ARCHIVOS DE GOOGLE ANALYTICS       --#
 
 #Une todos los archivos de productos de las cuentas.
-def Union_Archivos(Cuenta, analysis):
+def Union_Archivos(Cuenta, analysis = '', nivel = ''):
     
-    analytics_union = []
-    
-    for i in range(len(Cuenta)):
-        archivos = glob.glob("/home/carlos/Dropbox/Históricos GG/Históricos GG/" + Cuenta[i] + "/G Analytics/Productos/" + analysis + "/*.xlsx")
+    if nivel == 'Productos':
         
-        # Productos por Campaña #
-        for xls in archivos:
+        analytics_union = []
+    
+        for i in range(len(Cuenta)):
+            archivos = glob.glob("/home/carlos/Dropbox/Históricos GG/Históricos GG/" + Cuenta[i] + "/G Analytics/Productos/" + analysis + "/*.xlsx")
+            
+            # Productos por Campaña #
+            for xls in archivos:
                 tmp = pd.ExcelFile(xls)
                 tmp = pd.read_excel(xls, 'Conjunto de datos1')
                 tmp['archivo'] = xls
                 tmp = tmp.iloc[:-1]
                 tmp['cuenta'] = Cuenta[i]
                 analytics_union.append(tmp)
+                    
+        analytics_union = pd.concat(analytics_union)
 
-    analytics_union = pd.concat(analytics_union)
+        analytics_union['fechas'] = [ re.findall( r"\d{8}-\d{8}" ,i) for i in analytics_union.archivo ]
+                    
+    elif nivel == 'Audiencia':
+        
+        analytics_union = []
+        
+        for i in range(len(Cuenta)):
+            archivos = glob.glob("/home/carlos/Dropbox/Históricos GG/Históricos GG/" + Cuenta[i] + "/G Analytics/Audiencia/" + analysis + "/*.xlsx")
+            
+            # Productos por Campaña #
+            
+            for xls in archivos:
+                tmp = pd.ExcelFile(xls)
+                tmp = pd.read_excel(xls, 'Conjunto de datos1')
+                tmp['archivo'] = xls
+                tmp = tmp.iloc[:-1]
+                tmp['cuenta'] = Cuenta[i]
+                analytics_union.append(tmp)
+                
+        analytics_union = pd.concat(analytics_union)
 
-    analytics_union['fechas'] = [ re.findall( r"\d{8}-\d{8}" ,i) for i in analytics_union.archivo ]
-    
+        analytics_union['fechas'] = [ re.findall( r"\d{8}-\d{8}" ,i) for i in analytics_union.archivo ]
+                    
+    elif nivel == 'Tráfico':
+        
+        analytics_union = []
+        
+        for i in range(len(Cuenta)):
+            archivos = glob.glob("/home/carlos/Dropbox/Históricos GG/Históricos GG/" + Cuenta[i] + "/G Analytics/Tráfico/*.xlsx")
+            
+            # Productos por Campaña #
+            for xls in archivos:
+                    tmp = pd.ExcelFile(xls)
+                    tmp = pd.read_excel(xls, 'Conjunto de datos1')
+                    tmp['archivo'] = xls
+                    tmp = tmp.iloc[:-1]
+                    tmp['cuenta'] = Cuenta[i]
+                    analytics_union.append(tmp)
+        
+        analytics_union = pd.concat(analytics_union)
+
+        analytics_union['fechas'] = [ re.findall( r"\d{8}-\d{8}" ,i) for i in analytics_union.archivo ]
+        
     return analytics_union
+
+tmp = Union_Archivos(Cuenta = ['Office Depot'], analysis = 'segmentos_intencion_compra', nivel = 'Audiencia')
+analytics_productos = Union_Archivos(Cuenta = ['Office Depot','Petco','RadioShack','Home Store'], nivel = "Tráfico")
+analytics_productos = analytics_productos.fillna('vacio')
+tmp = analytics_productos.head()
+
+tmp
 
 
 ###############################################################################################
--- Union_Campañas/Base_ROAS.py
+++ Union_Campañas/Base_ROAS.py
@@ -17,6 +17,7 @@ Los Archivos se encuentran en: /home/carlos/Dropbox/ROAS 2020 (Cada mes se crea 
 
 @author: carlos
 """
+
 #Paqueterías
 import pandas as pd
 import os
@@ -31,17 +32,18 @@ from librerias import MP_FIC
 from librerias import Plataformas
 from librerias import Escritura_Sheets
 pd.set_option('display.float_format', lambda x: '%.10f' % x)
+
 #Aquí descargo el nuevo archivo de KPIS
 os.chdir('/home/carlos/Dropbox/ROAS 2020')
 Archivos = os.listdir()
 
 #-- MP_FIC (KPI) --#
 #La funcion MP_FIC_tabla que vive en el script MP_FIC nos ayuda a procesar el MP y FIC, descargado del google drive KPIS 2020
-Arch_MP_FIC = [x for x in Archivos if "KPI" in x]
+Arch_MP_FIC = [x for x in Archivos if "KPIS 2020 .xlsx" in x]
 #La función regresa una tupla con mp_fic, mp, fic
 tablas_mp_fic =  MP_FIC.MP_FIC_tabla(Arch_MP_FIC)
 
-mp_fic = tablas_mp_fic[0] 
+mp_fic = tablas_mp_fic[0]
 mp = tablas_mp_fic[1] 
 fic = tablas_mp_fic[2] 
 
@@ -92,8 +94,6 @@ duplicados = cuadrar[cuadrar.conteo > 1]
 cuadrar = cuadrar.groupby(['plataforma','plataforma_abreviacion'], as_index = False).sum()
 cuadrar.sum()
 
-a = mp_fic[(mp_fic.cliente == 'Petco') & (mp_fic.marca == 'Petco') & (mp_fic.mes_plan == 'Marzo') & (mp_fic.versión == 'V1') & (mp_fic.plataforma == 'Facebook')]
-
 #se le hace esta agrupacion para que no tengamos duplicados erroneos
 mp_fic = mp_fic.groupby(['cliente','cliente_nomenclatura','marca','campaña_nomenclatura','llave_ventas','llave_unica_mp','plataforma',
                           'plataforma_abreviacion','versión','fecha_inicio_plan','fecha_fin_plan','Año-Mes','mes_plan','tipo_presupuesto',
@@ -110,7 +110,7 @@ mp_fic = mp_fic.groupby(['cliente','cliente_nomenclatura','marca','campaña_nome
                                                                       'conteo_MP_FIC':'sum',
                                                                       'conteo':'sum'})
 
-
+tmp = mp_fic.groupby(['mes_plan','plataforma'], as_index = False).sum()
                           
 #-- Plataformas --#
 #Une los archivos de las plataformas que se dejan en las carpetas del dropbox, se pueden unir de forma semanal o mensual (la fecha de referencia será la fecha del reporte).
@@ -138,177 +138,141 @@ plataformas_02.plataforma.value_counts()
 plataformas_03.plataforma.value_counts()
 
 plataformas = pd.concat([plataformas_01,plataformas_02,plataformas_03])
+plataformas.mes.value_counts()
 
-del archivos_csv_01, archivos_csv_02, archivos_xlsx_01, archivos_xlsx_02, Arch_MP_FIC, Archivos
+del archivos_csv_01, archivos_csv_02, archivos_csv_03, archivos_xlsx_01, archivos_xlsx_02, archivos_xlsx_03, Arch_MP_FIC, Archivos, plataformas_01, plataformas_02, plataformas_03
 
 #a = plataformas_01[plataformas_01.llave_plataformas.str.contains('pachuca')]
 
-##############
-#Base Master
-#Aun se están haciendo pruebas para trabajar cada mes por separado, hacer una función que una todo dependiendo del mes#
-##############
-#plt
-#Enero
-mp_plt_01 = pd.merge(mp_fic, plataformas_01, how = 'left', left_on = 'llave_ventas', right_on = 'llave_plataformas')
 
-mp_plt_01['mes_cruze'] = 'Enero'
+#####################################################################################################
+#Base master Roas union
+#
+# Analisis de la union del mp y plataformas, división de casos (intersección, uniones y exclusiones) 
+#####################################################################################################
 
-bien_01 = mp_plt_01[(mp_plt_01.mes_plan == 'Enero') & ~(mp_plt_01.plataforma_y.isnull())]
-mal_01 = mp_plt_01[(mp_plt_01.mes_plan == 'Enero') & (mp_plt_01.plataforma_y.isnull())]
+tmp = pd.merge(mp_fic, plataformas, how = 'outer', left_on = 'llave_ventas', right_on = 'llave_plataformas')
 
-#Los que no cruzaron de enero
-mp_plt_01.mes_plan.value_counts()
+tmp['comentario'] = ""
 
-b = mp_plt_01[mp_plt_01.llave_ventas.str.contains('pachuca')]
-#'2001_GICSA_ExplanadaPachuca_PI_MKT_SEM_TRF' #Revisar si es correcto que este en 2 reportes
+#-Eliminamos filas que no tienen nada en las plataformas
+tmp_a = tmp[ (tmp.dinero_gastado != 0) | (tmp.impresiones != 0) | (tmp.conversiones != 0) | (tmp.revenue != 0)]
+#-Eliminamos filas que no tienen nada en las plataformas
+tmp_b = tmp[ (tmp.dinero_gastado == 0) & (tmp.impresiones == 0) & (tmp.conversiones == 0) & (tmp.revenue == 0)]
 
-#Febrero
-mp_plt_02 = pd.merge(mp_fic, plataformas_02, how = 'left', left_on = 'llave_ventas', right_on = 'llave_plataformas')
-mp_plt_02['mes_cruze'] = 'Febrero'
+#-Es lo que cruza correctamente
+tmp_c = tmp_a[ ~(tmp_a.llave_ventas.isnull()) & ~(tmp_a.llave_plataformas.isnull()) ]
+tmp_c['comentario'] = "información correcta, que tenemos en el mp y en las plataformas"
+#-Lo que debo revisar 
+tmp_d = tmp_a[ (tmp_a.llave_ventas.isnull()) | (tmp_a.llave_plataformas.isnull()) ]
 
-mp_plt_02.mes_plan.value_counts()
+#-Tengo en el mp y no las encontre en las plataformas
+tmp_d_1 = tmp_d[ tmp_d.llave_plataformas.isnull() ]
+ #Son unicas y no todas deben estar en las plataformas
+len(tmp_d_1.llave_unica_mp.unique())
+ #Dividimos en casos campañas de PV y ~PV
+tmp_d_1_1 = tmp_d_1[ ~(tmp_d_1.plataforma_abreviacion.str.contains('PV')) ] #Se tiene que revisar si se encuentra mal la nomenclatura en los reportes
+tmp_d_1_2 = tmp_d_1[ (tmp_d_1.plataforma_abreviacion.str.contains('PV')) ] #Se regresará a la base final ya que no contamos con datos de estas plataformas
+tmp_d_1_2['comentario'] = "información de provedores no contamos con reportes de plataformas"
 
-bien_02 = mp_plt_02[~mp_plt_02.plataforma_y.isnull()]
-mal_02 = mp_plt_02[(mp_plt_02.mes_plan == 'Febrero') & (mp_plt_02.plataforma_y.isnull())]
+#-Tengo en las plataformas y no las encontre en mp
+tmp_d_2 = tmp_d[ tmp_d.llave_ventas.isnull() ]
+ #Dividimos en casos 20 y ~20
+tmp_d_2_1 = tmp_d_2[ (tmp_d_2.llave_plataformas.str.contains('20')) ] #Se tiene que revisar si encuentra mal en el mp
+tmp_d_2_2 = tmp_d_2[ ~(tmp_d_2.llave_plataformas.str.contains('20')) ] #Se regresará a la base final ya que son datos buenos (conversiones asistidas, gereraron un gasto)
+tmp_d_2_2['comentario'] = "conversiones asistidas campañas que no tengo en el mp"
 
-c = mp_plt_02[mp_plt_02.llave_ventas.str.contains('pachuca')]
+##########################
+#¿Con que me debo quedar?
+tmp_union = pd.concat([ tmp_c, tmp_d_1_2, tmp_d_2_2 ])
 
-#Marzo
-mp_plt_03 = pd.merge(mp_fic, plataformas_03, how = 'left', left_on = 'llave_ventas', right_on = 'llave_plataformas')
-mp_plt_03['mes_cruze'] = 'Marzo'
+tmp_union = tmp_union.fillna('')
+tmp_union.mes_plan.value_counts()
 
-mp_plt_03.mes_plan.value_counts()
+tmp_union.cliente.value_counts()
 
-bien_03 = mp_plt_03[~mp_plt_03.plataforma_y.isnull()]
-mal_03 = mp_plt_03[(mp_plt_03.mes_plan == 'Marzo') & (mp_plt_03.plataforma_y.isnull())]
+########################################
+#¿Que formato final debe tener la base?
 
-d = mp_plt_03[mp_plt_03.llave_ventas.str.contains('pachuca')]
+tmp_union['dias_totales_campaña'] = 0
+tmp_union['semana'] = ''
+tmp_union['inversión_planeada'] = 0
+tmp_union['inversión_mp'] = 0
+tmp_union['inversión_diaria'] = 0
+tmp_union['ctr'] = 0
+tmp_union['llave_analytics'] = tmp_union['llave_ventas']
+tmp_union['total_conversiones'] = 0 
+tmp_union['total_revenue'] = 0
+tmp_union['roas'] = 0 
 
-union = pd.concat([bien_01, bien_02,bien_03])
+list(enumerate(tmp_union.keys()))
 
-union.mes_cruze.value_counts()
+tmp_union = tmp_union.iloc[:,[0,1,2,4,5,8,13,14,50,11,12,3,6,7,9,10,34,35,51,16,17,18,19,20,21,22,23,24,52,32,33,47,48,49,53,54,55,36,37,38,39,56,40,41,57,42,43,44,45,58,59,60]]
 
-union = union.fillna('')
+list(enumerate(tmp_union))
 
-FB = union[union.plataforma_abreviacion.str.contains('FB')]
-ADFORM = union[union.plataforma_abreviacion.str.contains('DSP')]
-ADWORDS = union[union.plataforma_abreviacion.str.contains('SEM')]
-
-a = ADWORDS[ADWORDS.llave_ventas.str.contains('pachuca')]
-
-union[union.cliente.str.contains('Depot')]
-union.cliente.value_counts()
-
-union['dias_totales_campaña'] = 0
-union['semana'] = ''
-union['inversión_planeada'] = 0
-union['inversión_mp'] = 0
-union['inversión_diaria'] = 0
-union['ctr'] = 0
-union['llave_analytics'] = union['llave_ventas']
-union['total_conversiones'] = 0 
-union['total_revenue'] = 0
-union['roas'] = 0 
-
-union = union.iloc[:,[0,1,2,4,5,8,13,14,50,11,3,6,7,9,10,34,35,51,16,17,18,19,20,21,22,23,24,52,32,33,47,48,49,53,54,55,36,37,38,39,56,40,41,57,42,43,44,45,58,59,60]]
-
-list(enumerate(union))
+FB_t = tmp_union[tmp_union.plataforma_abreviacion.str.contains('FB')]
+ADFORM_t = tmp_union[tmp_union.plataforma_abreviacion.str.contains('DSP')]
+ADWORDS_t = tmp_union[tmp_union.plataforma_abreviacion.str.contains('SEM')]
 
 #Separamos en version normal
-vn_union = union[~union.versión.str.contains('VC')]
+vn_union = tmp_union[~tmp_union.versión.str.contains('VC')]
 
 Escritura_Sheets.Escritura(vn_union , 2, Escribir = 'si', header = True, archivo_sheet = 'Base master Roas')
 
 #Separamos en version cliente
-vc_union = union[union.versión.str.contains('VC')]
+vc_union = tmp_union[tmp_union.versión.str.contains('VC')]
 
 Escritura_Sheets.Escritura(vc_union , 3, Escribir = 'si', header = True, archivo_sheet = 'Base master Roas')
-
-##########################################################
-#Cruzes para detectar errores de nomenclatura en mp ó plt#
-##########################################################
-
-#plt con mp
-plt_mp = pd.merge(plataformas, mp_fic, how = 'left', left_on = 'llave_plataformas', right_on = 'llave_ventas')
-plt_mp_no = plt_mp[pd.isnull(plt_mp.llave_ventas)]
-#¿Cuantos cruzaron?
-plt_mp = plt_mp[~pd.isnull(plt_mp.llave_ventas)]
-#mp con plt
-mp_plt = pd.merge(mp_fic, plataformas, how = 'left', left_on = 'llave_ventas', right_on = 'llave_plataformas')
-mp_plt_no = mp_plt[pd.isnull(mp_plt.llave_plataformas)]
-#¿Cuantos cruzaron? 
-mp_plt = mp_plt[~pd.isnull(mp_plt.llave_plataformas)]
 
 ###########################
 #Validacion de lo faltante#
 ###########################
-#plt_mp
-plt_mp_no_1 = plt_mp_no.llave_plataformas.str.split("_", 10,expand = True)
-plt_mp_no_1.columns = ["Año-Mes","Cliente","Marca","Tipo-1","Tipo-2","dummy"]
-plt_mp_no_1['dummy'] = ''
-plt_mp_no_1['archivo'] = 'plt_mp'
-plt_mp_no_1['Nombre_Campaña'] = plt_mp_no.llave_plataformas
 
-mp_plt_no_1 = mp_plt_no.llave_ventas.str.split("_", 10,expand = True)
-mp_plt_no_1.columns = ["Año-Mes","Cliente","Marca","Tipo-1","Tipo-2","dummy","dummy"]
-mp_plt_no_1 = mp_plt_no_1.iloc[:,[0,1,2,3,4,5]]
-mp_plt_no_1['archivo'] = 'mp_plt'
-mp_plt_no_1['Nombre_Campaña'] = mp_plt_no.llave_ventas
+##############################################
+#¿Qué debemos revisar sobre la nomenclatura? #
 
-Union = []
-Union.append(plt_mp_no_1)
-Union.append(mp_plt_no_1)
-Union = pd.concat(Union)
+a = tmp_d_2_1.llave_plataformas.str.split("_", 10,expand = True)
+a.columns = ["Año-Mes","Cliente","Marca","Tipo-1","Tipo-2","plataforma"]
+a['dummy'] = ''
+a['comentario'] = "Los tenemos en plataforma, se tiene que revisar si encuentra mal en el mp"
+a['Nombre_Campaña'] = tmp_d_2_1.llave_plataformas
+
+b = tmp_d_1_1.llave_ventas.str.split("_", 10,expand = True)
+b.columns = ["Año-Mes","Cliente","Marca","Tipo-1","Tipo-2","plataforma","dummy"]
+b.fillna('')
+b = b.iloc[:,[0,1,2,3,4,5,6]]
+b['comentario'] = 'Lo tenemos en el mp, se tiene que revisar si se encuentra mal la nomenclatura en los reportes'
+b['Nombre_Campaña'] = tmp_d_1_1.llave_ventas
+
+tmp_revision = pd.concat([a,b]) ; del a,b
+
+tmp_revision['conteo'] = 1
+tmp_revision.dummy = tmp_revision.dummy.apply(lambda x : str(x).replace('None',''))
+
+tmp_revision = tmp_revision.groupby(['Año-Mes', 'Cliente', 'Marca', 'Tipo-1', 'Tipo-2', 'plataforma',
+                                     'dummy', 'comentario', 'Nombre_Campaña'], as_index = False).count()
+
 
 #Desglose por cliente
-OD = Union.loc[ Union.Cliente.str.contains('od', na = False) & ~Union.Cliente.str.contains('sodexo', na = False)]
-RS = Union.loc[ Union.Cliente.str.contains('rs', na = False)]
-THS = Union.loc[ Union.Cliente.str.contains('ths', na = False)]
-PETCO = Union.loc[ Union.Cliente.str.contains('petco', na = False)]
-GICSA = Union.loc[ Union.Cliente.str.contains('gicsa', na = False)]
-GWEP = Union.loc[ Union.Cliente.str.contains('gwep', na = False)]
+OD = tmp_revision.loc[ tmp_revision.Cliente.str.contains('od', na = False) & ~tmp_revision.Cliente.str.contains('sodexo', na = False)]
+RS = tmp_revision.loc[ tmp_revision.Cliente.str.contains('rs', na = False)]
+THS = tmp_revision.loc[ tmp_revision.Cliente.str.contains('ths', na = False)]
+PETCO = tmp_revision.loc[ tmp_revision.Cliente.str.contains('petco', na = False)]
+GICSA = tmp_revision.loc[ tmp_revision.Cliente.str.contains('gicsa', na = False)]
+GWEP = tmp_revision.loc[ tmp_revision.Cliente.str.contains('gwep', na = False)]
 
 OD.shape[0] + RS.shape[0] + THS.shape[0] + PETCO.shape[0] + GICSA.shape[0] + GWEP.shape[0]
 
-OTROS = Union.loc[ ~Union.Cliente.str.contains('od', na = False) & ~Union.Cliente.str.contains('rs', na = False) & ~Union.Cliente.str.contains('sodexo', na = False) & ~Union.Cliente.str.contains('ths', na = False) &
-                  ~Union.Cliente.str.contains('petco', na = False) & ~Union.Cliente.str.contains('gicsa', na = False) & ~Union.Cliente.str.contains('gicsa', na = False) & ~Union.Cliente.str.contains('gwep', na = False)]
+OTROS = tmp_revision.loc[ ~tmp_revision.Cliente.str.contains('od', na = False) & ~tmp_revision.Cliente.str.contains('rs', na = False) & ~tmp_revision.Cliente.str.contains('sodexo', na = False) & ~tmp_revision.Cliente.str.contains('ths', na = False) &
+                  ~tmp_revision.Cliente.str.contains('petco', na = False) & ~tmp_revision.Cliente.str.contains('gicsa', na = False) & ~tmp_revision.Cliente.str.contains('gicsa', na = False) & ~tmp_revision.Cliente.str.contains('gwep', na = False)]
 
-#########################################
-#Escritura de los datos en Google Sheets#
-#########################################
-Escritura_Sheets.Escritura(OD, 12, header = True, Escribir = 'no')
-Escritura_Sheets.Escritura(RS, 13, header = True, Escribir = 'no')
-Escritura_Sheets.Escritura(THS, 14, header = True, Escribir = 'no')
-Escritura_Sheets.Escritura(PETCO, 15, header = True, Escribir = 'no')tmp = bien_01.groupby(['cliente','marca','mes_plan','versión','plataforma_x'], as_index = False).agg({
-                                                                                                'costo_planeado':'mean',
-                                                                                                'kpi_planeado':'mean',
-                                                                                                'serving':'mean',
-                                                                                                'inversión_plataforma':'mean',
-                                                                                                'inversión_total':'mean'})
+#Escritura de validaciones
+Escritura_Sheets.Escritura(OD, hoja = 0, header = 'si', Escribir = 'si', archivo_sheet = 'Validación Nomenclatura Base Roas')
+Escritura_Sheets.Escritura(RS, hoja = 1, header = 'si', Escribir = 'si', archivo_sheet = 'Validación Nomenclatura Base Roas')
+Escritura_Sheets.Escritura(THS, hoja = 2, header = 'si', Escribir = 'si', archivo_sheet = 'Validación Nomenclatura Base Roas')
+Escritura_Sheets.Escritura(PETCO, hoja = 3, header = 'si', Escribir = 'si', archivo_sheet = 'Validación Nomenclatura Base Roas')
+Escritura_Sheets.Escritura(GICSA, hoja = 4, header = 'si', Escribir = 'si', archivo_sheet = 'Validación Nomenclatura Base Roas')
+Escritura_Sheets.Escritura(GWEP, hoja = 5, header = 'si', Escribir = 'si', archivo_sheet = 'Validación Nomenclatura Base Roas')
 
-Escritura_Sheets.Escritura(GICSA, 16, header = True, Escribir = 'no')
-Escritura_Sheets.Escritura(GWEP, 17, header = True, Escribir = 'no')
-Escritura_Sheets.Escritura(OTROS, 18, header = True, Escribir = 'no')
-
-#Validaciones del momento (borrar)
-a = union[union.llave_plataformas.str.contains('2002_odcam')]
-
-tmp = union[union.mes_cruze == 'Febrero']
-
-
-#Validando mp y fic cifras
-bien_01.keys()
-
-tmp_t = bien_01[(bien_01.cliente == 'GICSA') & (bien_01.marca == 'Forum Cuernavaca')]
-
-
-tmp_c = tmp[tmp.cliente == 'The Home Store']
-
-tmp_t = tmp.groupby('cliente').sum()
-
-tmp.sum()
-
-tmp.plataforma_x.value_counts()
-
-#
-Escritura_Sheets.archivos_finales()
+Escritura_Sheets.Escritura(OTROS, hoja = 6, header = 'si', Escribir = 'si', archivo_sheet = 'Validación Nomenclatura Base Roas')
