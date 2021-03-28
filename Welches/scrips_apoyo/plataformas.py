b08ffcf4d449bd6f4f15364c22b0964cf847c91f
corderodedios182 <corderodedios182@gmail.com>
2021-03-27 22:38:25 -0600

    Base master V2.3

--- Welches/scrips_apoyo/plataformas.py
+++ Welches/scrips_apoyo/plataformas.py
@@ -4,6 +4,7 @@ Created on Sat Feb  6 20:06:54 2021
 
 @author: crf005r
 #Mejoras: 
+    - Incluir Aire
     - Importar los datos directo de Sheets
     - Limpiar y estructurar codigo repetitivo
     - Crear Validador (Lo que no cruza, Las que al abrir nomenclatura no encajan, Diagram de Ven Euler con los cruzes)
@@ -12,41 +13,53 @@ Created on Sat Feb  6 20:06:54 2021
 import os
 import pandas as pd
 from datetime import datetime
+pd.options.mode.chained_assignment = None
 
 os.chdir('C:/Users/crf005r/Documents/6_datasense')
 os.listdir()
 
-#--Facebook--#
 
-facebook = pd.read_excel('Facebook.xlsx', sheet_name = 'Raw Data Report')
-facebook = facebook.iloc[1:].loc[:,["Plataforma", "Nombre de la campaña", "Nombre del conjunto de anuncios", "Nombre del anuncio", "Día", "Divisa", "Clics en el enlace", "Impresiones", "Importe gastado (MXN)", "Reproducciones de video hasta el 100%", "Interacción con una publicación", "Alcance"]]
+#--Lectura de archivos--#
+facebook = pd.read_excel('Facebook.xlsx', sheet_name = 'Raw Data Report', skiprows=1,names=['Nombre de la campaña', 'Nombre del conjunto de anuncios','Nombre del anuncio', 'Plataforma', 'Día', 'Impresiones', 'Divisa','Importe gastado (MXN)', 'Alcance', 'Clics en el enlace','Reproducciones de video hasta el 100%','Interacción con una publicación', 'Inicio del informe','Fin del informe'])
+google_ads = pd.read_csv('Google Ads Plataforma.csv', skiprows = 2, encoding = "latin-1")
+
+#--Seleccion y renombrado de columnas--#
+facebook = facebook.loc[:,["Plataforma", "Nombre de la campaña", "Nombre del conjunto de anuncios", "Nombre del anuncio", "Día", "Divisa", "Clics en el enlace", "Impresiones", "Importe gastado (MXN)", "Reproducciones de video hasta el 100%", "Interacción con una publicación", "Alcance"]]
 facebook.columns = ["plataforma", "campaña", "grupo_de_anuncios", "anuncio", "fecha", "moneda", "clics", "impresiones", "dinero_gastado","views", "interacciones","alcance"]
+
+google_ads = google_ads.loc[:, ["Campaña", "Grupo de anuncios", "Día", "Moneda", "Clics", "Impresiones", "Costo", "Vistas"]]
+google_ads.columns = ["campaña", "grupo_de_anuncios", "fecha", "moneda", "clics", "impresiones", "dinero_gastado","views"]
+
+
+#--Formato de Columnas--#
+#Fechas
 facebook.fecha = pd.to_datetime(facebook.fecha, format = "%Y/%m/%d")
 facebook.fecha = facebook.fecha.apply(lambda x : x.strftime('%Y-%m-%d'))
-facebook.loc[(facebook['plataforma'].str.contains('audience_network')) | (facebook['plataforma'].str.contains('messenger')) , 'plataforma'] = 'facebook'
-facebook = facebook.groupby(['plataforma', 'campaña', 'grupo_de_anuncios', 'anuncio', 'fecha','moneda'], as_index = False).sum()
-facebook["llave_facebook"] = facebook.campaña + "-" + facebook.anuncio + "-" + facebook.fecha
-facebook["llave_facebook_tmp"] = facebook.campaña + "-" + facebook.anuncio + "-" + facebook.fecha + "-" + facebook.plataforma
 
-#Parte donde calculamos el porcentaje de dinero gastado de una campaña para Instagram y Facebook
-tmp = facebook[facebook.llave_facebook.str.contains('ECOM-FB-DPA_PROSPECTING-AON-010520-310520-TR-TRANSACCION_ONLINE-DPA-PROSPECTING-DINAMICO-COSMETICOS-2020-11-01')]
-tmp = facebook.groupby(['llave_facebook_tmp','llave_facebook']).agg({'dinero_gastado': 'sum'})
-tmp = tmp.groupby(level=1).apply(lambda x: 100 * x / float(x.sum()))
-tmp.columns = ['porcentaje_dinero_gastado']
-tmp_2 = pd.merge(facebook, tmp, how = 'outer', on = 'llave_facebook_tmp')
-
-facebook = tmp_2.loc[:,['plataforma', 'campaña', 'grupo_de_anuncios', 'anuncio','fecha','moneda', 'clics', 'impresiones', 'dinero_gastado', 'porcentaje_dinero_gastado', 'views','interacciones', 'alcance', 'llave_facebook']]
- 
-#--Google Ads--#
-
-google_ads = pd.read_csv('Google Ads Plataforma.csv', skiprows = 2, encoding = "latin-1")
-google_ads["Plataforma"] = 'Google Ads'
-google_ads["porcentaje_dinero_gastado"] = 1
-google_ads = google_ads.loc[:, ["Plataforma","Campaña", "Grupo de anuncios", "Día", "Moneda", "Clics", "Impresiones", "Costo", "porcentaje_dinero_gastado", "Vistas"]]
-google_ads.columns = ["plataforma", "campaña", "grupo_de_anuncios", "fecha", "moneda", "clics", "impresiones", "dinero_gastado","porcentaje_dinero_gastado","views"]
 google_ads.fecha = pd.to_datetime(google_ads.fecha, format = "%d/%m/%Y")
 google_ads.fecha = google_ads.fecha.apply(lambda x : x.strftime('%Y-%m-%d'))
-google_ads["llave_google"] = google_ads.campaña + "-" + google_ads.grupo_de_anuncios + "-" + google_ads.fecha
+
+#Numericas
+result = pd.concat([facebook, google_ads])
+
+result.clics = result.clics.apply(lambda x : str(x).replace(',','')).astype('float')
+result.impresiones = result.impresiones.apply(lambda x : str(x).replace(',','')).astype('float')
+result.dinero_gastado = result.dinero_gastado.apply(lambda x : str(x).replace(',','')).astype('float')
+result.views = result.views.apply(lambda x : str(x).replace(',','')).astype('float')
+result.interacciones = result.interacciones.apply(lambda x : str(x).replace(',','')).astype('float')
+result.alcance = result.alcance.apply(lambda x : str(x).replace(',','')).astype('float')
+result = result.fillna(0)
+
+#--Transformaciones--#
+#Etiquetado de datos, agrupaciones, campos calculados especiales
+
+result.loc[(result['plataforma'].str.contains('audience_network')) | (result['plataforma'].str.contains('messenger')) , 'plataforma'] = 'facebook'
+result.loc[result['plataforma'] == 0, 'plataforma'] = 'google ads'
+result = result.groupby(['plataforma', 'campaña', 'grupo_de_anuncios', 'anuncio', 'fecha','moneda'], as_index = False).sum()
+
+a = result.groupby(["plataforma","campaña","grupo_de_anuncios","anuncio","fecha"]).agg({'dinero_gastado': 'sum'})
+a = a.groupby(["campaña","grupo_de_anuncios","anuncio","fecha"]).apply(lambda x: x / float(x.sum())).reset_index().rename(columns = {"dinero_gastado":"porcentaje_dinero_gastado"})
+result = pd.merge(result, a, on = ["plataforma","campaña","grupo_de_anuncios","anuncio","fecha"])
 
 #--Analytics--#
 
@@ -55,23 +68,137 @@ analytics = analytics.reindex(columns = ['Campaña', 'Google Ads: grupo de anun
 analytics.columns = ["campaña", "grupo_de_anuncios", "anuncio", "fecha", "ingresos", "duracion_sesion", "sesiones", "usuarios", "usuarios_nuevos", "rebotes","paginas_vistas"]
 analytics.fecha = analytics.fecha.astype(str)
 analytics.fecha = analytics.fecha.apply(lambda x : datetime.strptime(x, '%Y%m%d').strftime('%Y-%m-%d'))
+analytics.campaña = analytics.campaña.apply(lambda x: str(x).replace("(not set)",""))
 analytics.grupo_de_anuncios = analytics.grupo_de_anuncios.apply(lambda x: str(x).replace("(not set)",""))
 analytics.anuncio = analytics.anuncio.apply(lambda x: str(x).replace("(not set)",""))
+
 analytics["llave_facebook"] =  analytics.campaña + "-" + analytics.anuncio + "-" + analytics.fecha
 analytics["llave_google"] =  analytics.campaña + "-" + analytics.grupo_de_anuncios + "-" + analytics.fecha
 analytics = analytics.loc[:,["llave_facebook","llave_google","ingresos", "duracion_sesion", "sesiones", "usuarios", "usuarios_nuevos", "rebotes","paginas_vistas"]]
 
-#--Cruzes de Informacion Faceboo, Google con Analytics--#
+#--Cruzes de Informacion Facebook, Google con Analytics--#
 
-tmp_1 = pd.merge(facebook, analytics, how = 'outer', left_on = "llave_facebook", right_on = "llave_facebook")
-tmp_1 = tmp_1.fillna(0)
-tmp_1 = tmp_1[(tmp_1.ingresos != 0) & (tmp_1.impresiones != 0)]
+#Facebook con Analytics
 
-tmp_2 = pd.merge(google_ads, analytics.iloc[:,1:], how = 'outer', left_on = "llave_google", right_on = "llave_google")
-tmp_2 = tmp_2.fillna(0)
-tmp_2 = tmp_2[(tmp_2.ingresos != 0) & (tmp_2.impresiones != 0)]
+#--Toda la información--#
+tmp = pd.merge(facebook, analytics, how = 'outer', on = ['campaña','anuncio','fecha'])
+tmp ['comentario'] = ""
 
-result = pd.concat([tmp_1, tmp_2])
+#-Generamos 2 casos para ver la calidad de informacion
+#Caso 1 queremos campañas que tengan algo en dinero gastado e ingresos
+tmp_a = tmp[ (tmp.dinero_gastado != 0) | (tmp.impresiones != 0) | (tmp.ingresos != 0) | (tmp.duracion_sesion != 0)]
+#Caso 2 muestra campañas basura que no gastaron ni generaron ingresos
+tmp_b = tmp[ (tmp.dinero_gastado == 0) & (tmp.impresiones == 0) & (tmp.ingresos == 0) & (tmp.duracion_sesion == 0)]
+
+#-Revisando lo que Cruza correctamente
+tmp_c = tmp_a[ ~(tmp_a.dinero_gastado.isnull()) & ~(tmp_a.ingresos.isnull()) ]
+tmp_c['comentario'] = "información correcta"
+
+#-Lo que debo revisar 
+tmp_d = tmp_a[ (tmp_a.dinero_gastado.isnull()) | (tmp_a.ingresos.isnull()) ]
+
+#-Tengo analytics y no las encontro en plataforma
+tmp_d_1 = tmp_d[ tmp_d.dinero_gastado.isnull() ]
+
+ #Son unicas y no todas deben estar en las plataformas
+len(tmp_d_1.llave_facebook.unique())
+ #Dividimos en casos campañas de Fb y Google
+tmp_d_1_1 = tmp_d_1[ ~(tmp_d_1.llave_facebook.str.contains('GA')) ] 
+tmp_d_1_2 = tmp_d_1[ (tmp_d_1.llave_facebook.str.contains('GA')) ] 
+tmp_d_1_2['comentario'] = "información que sabemos es de Google Ads"
+
+#-Tengo en las plataformas y no las encontre en analytics
+tmp_d_2 = tmp_d[ tmp_d.ingresos.isnull() ]
+
+#--¿Con que me debo quedar?--#
+#El objetivo actual es tener un monitoreo de las campañas.
+#Por lo tanto solo me quedo con campañas que cruzan bien.
+tmp_f = pd.concat([ tmp_c]) 
+tmp_f.comentario.value_counts()
+tmp_f = tmp_f.fillna(0)
+
+#Los valores unicos deben ser de información correcta, que tenemos en el mp y en las plataformasa
+tmp_f.llave_facebook.value_counts()
+validacion = tmp_f.groupby(['llave_facebook', 'plataforma']).count()
+#validacion es correcto ese duplicado por el grupo de anuncio
+tmp_f[tmp_f.llave_facebook.str.contains("COOP-FB-DECORACION_INTEGRAL-YVES_SAINT_LAURENT-201020-021120-AW-ENGAGEMENT-NATIVAS_LAL-COMPRADORES_DE_PERFUMERIA-STORIE-01-1920X1080-2020-11-02")]
+
+#--¿Toda la información cruzo?--#
+#
+# No, se debe revisar que es lo que no esta cruzando y ¿por que?
+# Una ves que se corrija volvemos a correr el código.
+#¿Qué debemos revisar sobre la nomenclatura? #
+tmp_revision = pd.concat([tmp_d_2,tmp_d_1_1])
+
+#Google con Analytics
+#Analisis de Google Ads
+google_ads["llave_google"] = google_ads.campaña + "-" + google_ads.grupo_de_anuncios + "-" + google_ads.fecha
+
+tmp = pd.merge(google_ads, analytics, how = 'outer', on = 'llave_google')
+tmp ['comentario'] = ""
+
+#-Generamos 2 casos para ver la calidad de informacion
+#Caso 1 queremos campañas que tengan algo en dinero gastado e ingresos
+tmp_a = tmp[ (tmp.dinero_gastado != 0) | (tmp.impresiones != 0) | (tmp.ingresos != 0) | (tmp.duracion_sesion != 0)]
+#Caso 2 muestra campañas basura que no gastaron ni generaron ingresos
+tmp_b = tmp[ (tmp.dinero_gastado == 0) & (tmp.impresiones == 0) & (tmp.ingresos == 0) & (tmp.duracion_sesion == 0)]
+
+#-Revisando lo que Cruza correctamente
+tmp_c = tmp_a[ ~(tmp_a.dinero_gastado.isnull()) & ~(tmp_a.ingresos.isnull()) ]
+tmp_c['comentario'] = "información correcta"
+
+#-Lo que debo revisar 
+tmp_d = tmp_a[ (tmp_a.dinero_gastado.isnull()) | (tmp_a.ingresos.isnull()) ]
+
+#-Tengo analytics y no las encontro en plataforma
+tmp_d_1 = tmp_d[ tmp_d.dinero_gastado.isnull() ]
+
+ #Son unicas y no todas deben estar en las plataformas
+len(tmp_d_1.llave_facebook.unique())
+ #Dividimos en casos campañas de Fb y Google
+tmp_d_1_1 = tmp_d_1[ ~(tmp_d_1.llave_facebook.str.contains('GA')) ] 
+tmp_d_1_2 = tmp_d_1[ (tmp_d_1.llave_facebook.str.contains('GA')) ] 
+tmp_d_1_2['comentario'] = "información que sabemos es de Google Ads"
+
+#-Tengo en las plataformas y no las encontre en analytics
+tmp_d_2 = tmp_d[ tmp_d.ingresos.isnull() ]
+
+#--¿Con que me debo quedar?--#
+#El objetivo actual es tener un monitoreo de las campañas.
+#Por lo tanto solo me quedo con campañas que cruzan bien.
+tmp_f = pd.concat([ tmp_c]) 
+tmp_f.comentario.value_counts()
+tmp_f = tmp_f.fillna(0)
+
+#Los valores unicos deben ser de información correcta, que tenemos en el mp y en las plataformasa
+tmp_f.llave_facebook.value_counts()
+validacion = tmp_f.groupby(['llave_facebook', 'plataforma']).count()
+#validacion es correcto ese duplicado por el grupo de anuncio
+tmp_f[tmp_f.llave_facebook.str.contains("COOP-FB-DECORACION_INTEGRAL-YVES_SAINT_LAURENT-201020-021120-AW-ENGAGEMENT-NATIVAS_LAL-COMPRADORES_DE_PERFUMERIA-STORIE-01-1920X1080-2020-11-02")]
+
+#--¿Toda la información cruzo?--#
+#
+# No, se debe revisar que es lo que no esta cruzando y ¿por que?
+# Una ves que se corrija volvemos a correr el código.
+#¿Qué debemos revisar sobre la nomenclatura? #
+tmp_revision = pd.concat([tmp_d_2,tmp_d_1_1])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+result = pd.concat([tmp_f, tmp_2])
 result["ingresos_val"] = result.ingresos
 result = result.loc[:,["plataforma", "campaña", "grupo_de_anuncios", "anuncio", "fecha", "moneda", "clics", "impresiones", "dinero_gastado", "porcentaje_dinero_gastado","ingresos", "ingresos_val", "duracion_sesion", "sesiones", "usuarios", "usuarios_nuevos", "rebotes", "views", "interacciones", "paginas_vistas", "alcance"]]
 result = result.fillna(0)
@@ -114,5 +241,5 @@ tmp_f = pd.concat([tmp_1,tmp_2,tmp_3,result], axis = 1)
 
 tmp_f.to_csv('master_2_python.csv')
 
-
+#
 
